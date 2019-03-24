90981b7fc844e8cc490b43f9c301e39ce7b357c1
Josh Price <joshua.price88@gmail.com>
2019-03-24 07:28:10 -0500

    Finished styling site, added JS for the click and scroll actions

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+//Creating the Menu Click Function
 $(document).ready(function () {
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
-    })
-})
+    });
+    //causes nav page to close with click on link
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    //page will scroll to what section is clicked
+    $('a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        },  1500);
+    });
+    //scrolls back to top of page after click.
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        },  800);
+    });
+
+});
